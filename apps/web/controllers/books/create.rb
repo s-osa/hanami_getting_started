5876c9cabb145be3fa52d35838b305b8be1f26f9
OSA Shunsuke <shunsuke-osa@cookpad.com>
2017-08-30 20:44:39 +0900

    Refactor routes

--- apps/web/controllers/books/create.rb
+++ apps/web/controllers/books/create.rb
@@ -16,7 +16,7 @@ module Web::Controllers::Books
         repo = BookRepository.new
         repo.create(params[:book])
 
-        redirect_to '/books'
+        redirect_to routes.books_path
       else
         self.status = 422
       end
